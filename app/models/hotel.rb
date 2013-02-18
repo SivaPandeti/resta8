1090e811d0c8751d7e6c2da50647be850048e702
Siva Pandeti <psivah@yahoo.com>
2013-02-18 03:31:54 -0500

    change home page to hotels1

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,4 +2,5 @@ class Hotel < ActiveRecord::Base
   attr_accessible :address, :city, :email, :hotel_name, :phone, :website
 
   has_many :rooms, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end
